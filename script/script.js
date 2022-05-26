200c05f87be2a40e8d2f99e3f352383a7d378225
Ivan Ivaniuk <75336486+Ivan-juni@users.noreply.github.com>
2022-05-26 22:04:50 +0300

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -6,3 +6,9 @@ let changed = document.querySelector('.wrapper__caption');
 console.log(window.innerWidth);
 
 btn.onclick = () => changed.style.display = (changed.style.display == "none") ? "flex" : "none";
+
+// btn.onclick = () => document.body.classList.toggle('_lock'); // для блокирования скролла при открытом меню
+
+btn.addEventListener("click", function (e) {
+    document.body.classList.toggle('_lock'); // для блокирования скролла при открытом меню
+})
