8c2e1e2e8e1d75c7c19deeec2256a96532445558
Ivan Ivaniuk <75336486+Ivan-juni@users.noreply.github.com>
2022-05-30 13:13:53 +0300

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,4 +1,3 @@
-
 let btn = document.querySelector('.navbar-toggler');
 let changed = document.querySelector('.wrapper__caption');
 
@@ -12,3 +11,40 @@ btn.onclick = () => changed.style.display = (changed.style.display == "none") ? 
 btn.addEventListener("click", function (e) {
     document.body.classList.toggle('_lock'); // для блокирования скролла при открытом меню
 })
+
+
+// input.addEventListener("click", function (e) {
+//     sel.classList.toggle('disabled');
+// })
+
+function toggleButton() {
+    let username = document.getElementById('firstname').value;
+    let email = document.getElementById('email').value;
+
+    if (username && email) {
+        document.getElementById('submitButton').disabled = false;
+        document.getElementById('submitButton').classList.remove('disabled');
+    } else {
+        document.getElementById('submitButton').disabled = true;
+        document.getElementById('submitButton').classList.add('disabled');
+    }
+}
+
+function toggleInputOther() {
+    const inp = document.querySelector('.input__other');
+    const sel = document.getElementById("form__select");
+
+    if (inp.value !== '') {
+        sel.classList.add('disabled');
+        sel.disabled = true;
+    } else {
+        sel.classList.remove('disabled');
+        sel.disabled = false;
+    }
+}
+
+// $(document).ready(function () {
+// });
+
+
+
