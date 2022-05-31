5fb9110f8bc93f356fe3d401d78a3ac59124185e
Ivan Ivaniuk <75336486+Ivan-juni@users.noreply.github.com>
2022-05-31 13:44:02 +0300

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,21 +1,21 @@
+// ? бургер - меню
+
 let btn = document.querySelector('.navbar-toggler');
 let changed = document.querySelector('.wrapper__caption');
 
+// console.log(window.innerWidth);
 
-console.log(window.innerWidth);
-
-btn.onclick = () => changed.style.display = (changed.style.display == "none") ? "flex" : "none";
-
-// btn.onclick = () => document.body.classList.toggle('_lock'); // для блокирования скролла при открытом меню
-
-btn.addEventListener("click", function (e) {
-    document.body.classList.toggle('_lock'); // для блокирования скролла при открытом меню
-})
+// btn.onclick = () => changed.style.display = (changed.style.display == "none") ? "flex" : "none";
 
 
-// input.addEventListener("click", function (e) {
-//     sel.classList.toggle('disabled');
+
+// btn.addEventListener("click", function (e) {
+//     document.body.classList.toggle('_lock'); // для блокирования скролла при открытом меню
 // })
+
+// ?
+
+//! Функции для взаимодействия с формой 
 
 function toggleButton() {
     let username = document.getElementById('firstname').value;
@@ -42,9 +42,25 @@ function toggleInputOther() {
         sel.disabled = false;
     }
 }
+// !
 
-// $(document).ready(function () {
-// });
+// * Vue *
+
+
+var app = new Vue({
+    el: "#app",
+    data: {
+        products: [
+            { id: 1, title: "TAG 1000 (TAG 853)", short_text: "Lemon Determinate Yellow Standard Round", image: 'lemons-1.jpg', desc: "Full desc" },
+            { id: 2, title: "TAG 1001 (TAG 855)", short_text: "Lemon Determinate Yellow Standard Round", image: 'lemons-2.jpeg', desc: "Full desc" },
+            { id: 3, title: "TAG 1002 (TAG 809)", short_text: "Lemon Determinate Yellow Standard Round", image: 'lemons-3.jpeg', desc: "Full desc" },
+            { id: 4, title: "TAG 1003 (TAG 834)", short_text: "Lemon Determinate Yellow Standard Round", image: 'lemons-4.jpeg', desc: "Full desc" },
+            { id: 5, title: "TAG 1004 (TAG 848)", short_text: "Lemon Determinate Yellow Standard Round", image: 'lemons-5.jpg', desc: "Full desc" },
+        ]
+    },
+})
+
+// *     *
 
 
 
